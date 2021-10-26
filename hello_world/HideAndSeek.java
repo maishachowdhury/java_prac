87b818a7b3c2e7e67b00c61d49f40018534e93b2
Maisha <maisha-1998@hotmail.co.uk>
2021-10-26 10:48:01 +0100

    noted added

--- hello_world/HideAndSeek.java
+++ hello_world/HideAndSeek.java
@@ -1,6 +1,6 @@
 public class HideAndSeek {
   public static void main(String[] args) {
-    System.out.println("Let's play hide and seek.");
+     System.out.println("Let's play hide and seek.");
      System.out.print("Three...");
      System.out.print("Two...");
 
@@ -8,3 +8,5 @@ public class HideAndSeek {
   }
 }
 
+// System.out.println prints the string on a new line - system output print line 
+// System.out.print prints string on same line 
