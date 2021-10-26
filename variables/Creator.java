45a1ece542447644b4713b00e43dfc81ab12dc97
Maisha <maisha-1998@hotmail.co.uk>
2021-10-26 12:55:13 +0100

    variables into

---
+++ variables/Creator.java
@@ -0,0 +1,17 @@
+public class Creator {
+	public static void main(String[] args) {
+    String name = "James Gosling";
+    int yearCreated = 1995;
+    System.out.println(name);
+    System.out.println(yearCreated);
+	}
+}
+
+// public class Creator - name of the class is Creator 
+// { } open curly braces to open the scope of the methods
+// add in main as it is the entry point 
+// the variable name is declared a string and contains a string
+// yearCreated is declared an int and stores a number without decimals 
+// system.out.println prints the the contents of the variable on each line 
+// { } curly braces to close off scope of the methods then again to close off the 
+// scope of the class 
