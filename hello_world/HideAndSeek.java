a7960f0c213a34a762e6b3238f6fa25d9779d1cc
Maisha <maisha-1998@hotmail.co.uk>
2021-10-26 10:49:55 +0100

    new lines added

--- hello_world/HideAndSeek.java
+++ hello_world/HideAndSeek.java
@@ -3,8 +3,8 @@ public class HideAndSeek {
      System.out.println("Let's play hide and seek.");
      System.out.print("Three...");
      System.out.print("Two...");
-
-
+     System.out.println("One...");
+     System.out.println("Ready or not, here I come!");
   }
 }
 
